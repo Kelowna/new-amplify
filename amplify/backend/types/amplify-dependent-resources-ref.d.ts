d8825b8b6f6e0934f69bc0e1652a49bd6929482a
Kelowna Vincent <kelowna@Kelownas-MacBook-Air.local>
2022-10-09 16:21:44 -0400

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyapp6d3f4d6d": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,6 +18,12 @@
  * BROWSER POLYFILLS
  */
 
+declare global {
+  interface Window { global: any; }
+}
+window.global = window;
+
+
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
  * user can disable parts of macroTask/DomEvents patch by setting following flags
