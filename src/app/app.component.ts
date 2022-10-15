a7d1f2cfea56015954cea397f3cc1b67c482cc54
System Administrator <root@Kelownas-MacBook-Air.local>
2022-10-14 22:29:22 -0400

    Updating Forms II

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { APIService, Restaurant } from './API.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'amplify-app';
+  public createForm: FormGroup;
+
+  public restaurants: Array<Restaurant> = [];
+
+  constructor(private api: APIService, private fb: FormBuilder) {
+    this.createForm = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators,required],
+      city: ['', Validators.required]
+    });
+  }
+
+  async ngOnInit() {
+    this.api.ListRestaurants().then((event) => {
+      this.restaurants = event.items as Restaurant[];
+    });
+  }
+
+
+  public onCreate(restaurant: Restaurant) {
+    this.api
+      .CreateRestaurant(restaurant)
+      .then((event) => {
+        console.log('item created!');
+        this.createForm.reset();
+      })
+      .catch((e) => {
+        console.log('error creating restaurant...', e);
+      });
+  }
+}
+
+
 }
