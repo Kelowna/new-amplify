c4b888956bdff57362e85eb5601d5c85b2b379c5
System Administrator <root@Kelownas-MacBook-Air.local>
2022-10-15 21:52:44 -0400

    Added restaurant data stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurantsComponent
   ],
   imports: [
     BrowserModule,
