3cde71928855be98b9d655a6b6e5a84c2f3f70b9
System Administrator <root@Kelownas-MacBook-Air.local>
2022-10-15 22:13:00 -0400

    Restaurants error fix attempt

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -2,16 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { APIService, Restaurant } from '../API.service';
 
-
 @Component({
   selector: 'app-restaurants',
   templateUrl: './restaurants.component.html',
   styleUrls: ['./restaurants.component.css']
 })
-
-
 export class RestaurantsComponent implements OnInit {
   public createForm: FormGroup;
+
+  /* declare restaurants variable */
+  public restaurants: Array<Restaurant> = [];
 
   constructor(private api: APIService, private fb: FormBuilder) {
     this.createForm = this.fb.group({
@@ -22,16 +22,16 @@ export class RestaurantsComponent implements OnInit {
   }
 
   async ngOnInit() {
+    /* fetch restaurants when app loads */
     this.api.ListRestaurants().then((event) => {
       this.restaurants = event.items as Restaurant[];
     });
   }
 
-
   public onCreate(restaurant: Restaurant) {
     this.api
       .CreateRestaurant(restaurant)
-      .then(() => {
+      .then((event) => {
         console.log('item created!');
         this.createForm.reset();
       })
@@ -40,6 +40,3 @@ export class RestaurantsComponent implements OnInit {
       });
   }
 }
-
-
-
